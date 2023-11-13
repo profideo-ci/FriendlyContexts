a3caf1af34963885d3b56428e6baa8710a66de8a
Florent SEVESTRE <aniki.taicho@gmail.com>
2023-11-13 17:21:37 +0100

    Fix Alice fixtures loader

--- src/Knp/FriendlyContexts/Alice/Fixtures/Loader.php
+++ src/Knp/FriendlyContexts/Alice/Fixtures/Loader.php
@@ -3,47 +3,12 @@
 namespace Knp\FriendlyContexts\Alice\Fixtures;
 
 use Knp\FriendlyContexts\Alice\ProviderResolver;
-use App\Faker\Loader\NativeLoader as BaseLoader;
+use Nelmio\Alice\Loader\NativeLoader as BaseLoader;
 use Nelmio\Alice\DataLoaderInterface;
 use Nelmio\Alice\FixtureBuilderInterface;
 use Nelmio\Alice\GeneratorInterface;
 use Nelmio\Alice\IsAServiceTrait;
 use Nelmio\Alice\ObjectSet;
-
-class SimpleDataLoader implements DataLoaderInterface
-{
-    use IsAServiceTrait;
-
-    /**
-     * @var FixtureBuilderInterface
-     */
-    private $builder;
-
-    /**
-     * @var GeneratorInterface
-     */
-    private $generator;
-
-    public function __construct(FixtureBuilderInterface $fixtureBuilder, GeneratorInterface $generator)
-    {
-        $this->builder = $fixtureBuilder;
-        $this->generator = $generator;
-    }
-
-    public $fixtureSet;
-
-    /**
-     * @inheritdoc
-     */
-    public function loadData(array $data, array $parameters = [], array $objects = []) : ObjectSet
-    {
-        $fixtureSet = $this->builder->build($data, $parameters, $objects);
-        $this->fixtureSet = $fixtureSet;
-
-        return $this->generator->generate($fixtureSet);
-    }
-}
-
 
 class Loader extends BaseLoader
 {
